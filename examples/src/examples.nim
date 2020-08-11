6799390a8a68d16aef2e09ba1638da2812225fa2
oakes <zach@sekao.net>
2020-08-11 16:14:59 -0400

    Break example code into separate procs

--- examples/src/examples.nim
+++ examples/src/examples.nim
@@ -4,30 +4,11 @@ import parasound/miniaudio
 import paramidi_soundfonts
 import os
 
-when isMainModule:
-  const
-    sampleRate = 44100
-    numSamples = 1 * sampleRate
-  var sf = tsf_load_filename(paramidi_soundfonts.getSoundFontPath("aspirin.sf2"))
-  # if you want to embed the soundfont in the binary, do this instead:
-  #const soundfont = staticRead("paramidi_soundfonts/aspirin.sf2")
-  #var sf = tsf_load_memory(soundfont.cstring, soundfont.len.cint)
-  tsf_set_output(sf, TSF_MONO, sampleRate, 0) #sample rate
-  tsf_note_on(sf, 0, 60, 1.0f) #preset 0, middle C
-  var sec: array[numSamples, cshort] # synthesize 1 second
-  tsf_render_short(sf, cast[ptr cshort](sec.addr), sec.len.cint, 0)
+const
+  sampleRate = 44100
+  numSamples = 1 * sampleRate
 
-  var wav: drwav
-  var format: drwav_data_format
-  format.container = drwav_container_riff
-  format.format = DR_WAVE_FORMAT_PCM
-  format.channels = 1
-  format.sampleRate = sampleRate
-  format.bitsPerSample = 16
-  doAssert drwav_init_file_write(wav.addr, "output.wav", addr(format), nil)
-  doAssert numSamples == drwav_write_pcm_frames(wav.addr, numSamples, sec.addr)
-  discard drwav_uninit(wav.addr)
-
+proc playFile(filename: string, sleepMsecs: int) =
   var
     decoder = newSeq[uint8](ma_decoder_size())
     decoderAddr = cast[ptr ma_decoder](decoder[0].addr)
@@ -35,7 +16,7 @@ when isMainModule:
     deviceConfigAddr = cast[ptr ma_device_config](deviceConfig[0].addr)
     device = newSeq[uint8](ma_device_size())
     deviceAddr = cast[ptr ma_device](device[0].addr)
-  doAssert MA_SUCCESS == ma_decoder_init_file("output.wav", nil, decoderAddr)
+  doAssert MA_SUCCESS == ma_decoder_init_file(filename, nil, decoderAddr)
 
   proc data_callback(pDevice: ptr ma_device; pOutput: pointer; pInput: pointer; frameCount: ma_uint32) {.cdecl.} =
     let decoderAddr = ma_device_get_decoder(pDevice)
@@ -51,7 +32,31 @@ when isMainModule:
     discard ma_decoder_uninit(decoderAddr)
     quit("Failed to start playback device.")
 
-  sleep(2000)
+  sleep(sleepMsecs)
   discard ma_device_stop(deviceAddr)
   #ma_device_uninit(deviceAddr)
   discard ma_decoder_uninit(decoderAddr)
+
+proc writeFile(filename: string, data: var openArray[cshort]) =
+  var wav: drwav
+  var format: drwav_data_format
+  format.container = drwav_container_riff
+  format.format = DR_WAVE_FORMAT_PCM
+  format.channels = 1
+  format.sampleRate = sampleRate
+  format.bitsPerSample = 16
+  doAssert drwav_init_file_write(wav.addr, filename, addr(format), nil)
+  doAssert numSamples == drwav_write_pcm_frames(wav.addr, numSamples, data.addr)
+  discard drwav_uninit(wav.addr)
+
+when isMainModule:
+  var sf = tsf_load_filename(paramidi_soundfonts.getSoundFontPath("aspirin.sf2"))
+  # if you want to embed the soundfont in the binary, do this instead:
+  #const soundfont = staticRead("paramidi_soundfonts/aspirin.sf2")
+  #var sf = tsf_load_memory(soundfont.cstring, soundfont.len.cint)
+  tsf_set_output(sf, TSF_MONO, sampleRate, 0) #sample rate
+  tsf_note_on(sf, 0, 60, 1.0f) #preset 0, middle C
+  var data: array[numSamples, cshort] # synthesize 1 second
+  tsf_render_short(sf, cast[ptr cshort](data.addr), data.len.cint, 0)
+  writeFile("output.wav", data)
+  playFile("output.wav", 1000)
