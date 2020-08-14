00c22df2a9a50ca97397c24944be784f37d24e4f
oakes <zach@sekao.net>
2020-08-14 19:35:58 -0400

    Remove duplicate measure constant

--- examples/src/dueling_banjos.nim
+++ examples/src/dueling_banjos.nim
@@ -8,10 +8,9 @@ const
   measure2 = (1/16, g, g, 1/8, g, a, b, +c, +d, +c, 1/2, b)
   measure3 = (1/16, {d, -b, -g}, {d, -b, -g},
               1/8, {d, -b, -g}, {e, c, -g}, {d, -b, -g})
-  measure4 = (1/16, b, +c, 1/8, +d, b, +c, a, b, g, a)
-  measure5 = (1/16, r, r, 1/8, g, r, d, r, g, g, d)
-  measure6 = (1/4, g, 1/8, a, b, 1/4, g, 1/8, a, d)
-  measure7 = (1/4, g, 1/8, a, b, 1/4, g, 1/8, {f, -a}, b, 1/4, c)
+  measure4 = (1/16, r, r, 1/8, g, r, d, r, g, g, d)
+  measure5 = (1/4, g, 1/8, a, b, 1/4, g, 1/8, a, d)
+  measure6 = (1/4, g, 1/8, a, b, 1/4, g, 1/8, {f, -a}, b, 1/4, c)
   score =
     ((tempo: 80, octave: 3),
      (guitar, measure1),
@@ -34,17 +33,17 @@ const
      (banjo, (octave: 3), measure1),
      (tempo: 120, octave: 3),
      ((mode: concurrent),
-      (banjo, measure4),
+      (banjo, measure1),
+      (guitar, measure4)),
+     ((mode: concurrent),
+      (banjo, measure1),
+      (guitar, measure4)),
+     ((mode: concurrent),
+      (banjo, measure1),
       (guitar, measure5)),
      ((mode: concurrent),
-      (banjo, measure4),
-      (guitar, measure5)),
-     ((mode: concurrent),
-      (banjo, measure4),
+      (banjo, measure1),
       (guitar, measure6)),
-     ((mode: concurrent),
-      (banjo, measure4),
-      (guitar, measure7)),
      1/2, r)
 
 when isMainModule:
