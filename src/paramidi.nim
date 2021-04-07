083ab75cc7842d949f5ea98514d9ab97bc524c35
oakes <zach@sekao.net>
2021-04-07 17:30:26 +0200

    Move json code into main module

--- src/paramidi.nim
+++ src/paramidi.nim
@@ -1,6 +1,8 @@
 from sequtils import nil
 from algorithm import nil
 import paramidi/tsf
+from paramidi/constants import nil
+import json, sets
 
 type
   Note* = enum
@@ -627,7 +629,72 @@ proc compile(ctx: var Context, content: tuple) =
   else:
     ctx.time = temp.time
 
-include paramidi/json
+proc compile*(ctx: var Context, node: JsonNode) =
+  case node.kind:
+  of JString:
+    if constants.noteSet.contains(node.str):
+      compile(ctx, Note(constants.notes.find(node.str)))
+    elif constants.instruments.contains(node.str):
+      compile(ctx, Instrument(constants.instruments.find(node.str)))
+    else:
+      raise newException(Exception, "Invalid value: " & $node & " (expected a note or instrument)")
+  of JInt:
+    compile(ctx, node.num)
+  of JFloat:
+    compile(ctx, node.fnum)
+  of JBool, JNull:
+    raise newException(Exception, "Invalid value: " & $node)
+  of JObject:
+    for k, v in node:
+      if k == "length":
+        case v.kind:
+        of JInt:
+          setLength(ctx, v.num)
+        of JFloat:
+          setLength(ctx, v.fnum)
+        else:
+          raise newException(Exception, "Invalid length: " & $v)
+      elif k == "octave":
+        if v.kind == JInt:
+          ctx.octave = v.num
+        else:
+          raise newException(Exception, "Invalid octave: " & $v)
+      elif k == "play":
+        if v.kind == JBool:
+          ctx.play = v.bval
+        else:
+          raise newException(Exception, "Invalid play: " & $v)
+      elif k == "mode":
+        if v.kind == JString and constants.modeSet.contains(v.str):
+          ctx.mode = Mode(constants.modes.find(v.str))
+        else:
+          raise newException(Exception, "Invalid mode: " & $v)
+      elif k == "tempo":
+        if v.kind == JInt:
+          ctx.tempo = v.num.int
+        else:
+          raise newException(Exception, "Invalid tempo: " & $v)
+      else:
+        raise newException(Exception, "Invalid attribute: " & k)
+  of JArray:
+    var
+      temp = ctx
+      concurrent = false
+      longestTime = ctx.time
+    for item in node:
+      let mode = temp.mode
+      compile(temp, item)
+      if mode != temp.mode and temp.mode == Mode.concurrent:
+        concurrent = true
+        temp.mode = Mode.sequential
+      if concurrent:
+        if temp.time > longestTime:
+          longestTime = temp.time
+        temp.time = ctx.time
+    if concurrent:
+      ctx.time = longestTime
+    else:
+      ctx.time = temp.time
 
 proc compile*(content: tuple | JsonNode): seq[Event] =
   var ctx = Context(
