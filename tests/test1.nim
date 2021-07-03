3ce7cce43c5d6e4e59f6a4b934fe57d4e8754310
oakes <zach@sekao.net>
2021-07-03 09:39:51 +0200

    Use source filter to support flat notes

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,3 +1,5 @@
+#? replace(sub="d♭", by="cx") | replace(sub="e♭", by="dx") | replace(sub="g♭", by="fx") | replace(sub="a♭", by="gx") | replace(sub="b♭", by="ax")
+
 import unittest
 import paramidi, paramidi/tsf
 from os import nil
@@ -10,6 +12,12 @@ test "load soundfont":
   tsf_note_on(sf, 0, 60, 1.0f) #preset 0, middle C
   var halfSecond: array[22050, cshort] # synthesize 0.5 seconds
   tsf_render_short(sf, cast[ptr cshort](halfSecond.addr), halfSecond.len.cint, 0)
+
+test "sharps and flats":
+  var
+    score1 = compile((piano, 1/4, cx, dx, fx, gx, ax, cx+1, dx+1, fx+1, gx+1, ax+1))
+    score2 = compile((piano, 1/4, d♭, e♭, g♭, a♭, b♭, d♭+1, e♭+1, g♭+1, a♭+1, b♭+1))
+  check score1 == score2
 
 from algorithm import nil
 
